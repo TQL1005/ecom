aeb9c8039cd9bad9dc6f6d1f052405179ebe5be9
Trinh Quang Loc <loc_trinh_quang@bita.jp>
2024-12-10 18:21:40 +0700

    add count

--- python/ecom/static/js/cart.js
+++ python/ecom/static/js/cart.js
@@ -1,4 +1,5 @@
 var btnCart = document.getElementsByClassName("update-cart");
+let x = 0;
 for (let index = 0; index < btnCart.length; index++) {
     btnCart[index].addEventListener("click", myFunc);
 }
@@ -6,4 +7,22 @@ function myFunc(event) {
     var productId = event.target.getAttribute('data-product');
     var action = event.target.getAttribute('data-action');
     console.log("product " + productId + " action " + action);
+    let x = 0;
+    if (sessionStorage.getItem(productId)){
+        x = parseInt(sessionStorage.getItem(productId));
+    }else{
+        x = 0;
+    }
+    if (action){
+        x = x + 1; 
+    }
+    sessionStorage.setItem(productId,x);
+
+    if (sessionStorage.clickcount) {
+        sessionStorage.clickcount = Number(sessionStorage.clickcount) + 1;
+        document.getElementById("cart-total").innerHTML = sessionStorage.clickcount
+      } else {
+        sessionStorage.clickcount = 1;
+      }
 }
+
