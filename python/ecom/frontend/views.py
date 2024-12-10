43aad503f7837ee209334fbb33e084b781495717
Trinh Quang Loc <loc_trinh_quang@bita.jp>
2024-12-10 15:59:19 +0700

    fixing push data to server side instead of clien side

--- python/ecom/frontend/views.py
+++ python/ecom/frontend/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
-
+import requests
 # Create your views here.
-def index(request):
+def index(request):   
     return render(request,'index.html')
 
 def store(request):
-    return render(request,'store.html')
+    r = requests.get('http://127.0.0.1:8000/api/product')
+    products = []
+    for x in r.json():
+        products.append({
+            'name': x['name'],
+            'price': x['price'],
+            'images': x['images']
+        })
+    context = {'products': products}
+    return render(request,'store.html',context=context)
 
 def login(request):
     return render(request,'login.html')
