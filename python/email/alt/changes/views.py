36553501a5fb473840c767edd00d624ac3a231a5
Trinh Quang Loc <loc_trinh_quang@bita.jp>
2024-12-10 14:33:51 +0700

    add static

--- python/email/alt/changes/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-
-def members(request):
-    return render(request,"alt/index.html")
-
--- python/mail-failed/change_alt/api/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Member
-
-class Serializer(serializers.ModelSerializer):
-    class Meta:
-        model = Member
-        fields = "__all__"
