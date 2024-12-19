962cda5e8e16fb35797419f508ea400f1d75378b
TQL1005 <86201052+TQL1005@users.noreply.github.com>
2024-12-19 18:52:58 +0700

    Update supabase_client.py

--- python/ecom/api/supabase_client.py
+++ python/ecom/api/supabase_client.py
@@ -2,7 +2,7 @@ from supabase import create_client
 
 # Your Supabase configuration
 SUPABASE_URL = "https://miwagyoykpqvocoiczvu.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1pd2FneW95a3Bxdm9jb2ljenZ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM3MDg0MDAsImV4cCI6MjA0OTI4NDQwMH0.B7EXzIlX3FQfxl4HwSFiOuhAUiKOeaG0x5itrF0bIFo"
+SUPABASE_KEY = "YOUR_KEY"
 
 # Create and export the client
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
